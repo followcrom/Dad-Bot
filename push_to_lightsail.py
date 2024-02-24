a1b82c998e00eb470da313585210ef5823a6652b
followCrom <followcrom@gmail.com>
2024-02-24 12:09:01 +0000

    Update Lightsail deployment configuration and push script

--- push_to_lightsail.py
+++ push_to_lightsail.py
@@ -1,16 +1,31 @@
 import boto3
 
 # Set up Lightsail client
-lightsail = boto3.client("lightsail")
+lightsail = boto3.client("lightsail", region_name="your-region")
 
-# Image details (replace with your image details)
-docker_hub_image = "followcrom/dad_bot"
-image_tag = "latest"
-container_service_name = "dad-bot-cont"
 
-# Push image to Lightsail
-lightsail.push_container_image(
-    service_name=container_service_name,
-    label=image_tag,
-    image=docker_hub_image + ":" + image_tag,
+deployment = {
+    'containers': {
+        'your-container-name': {
+            'image': "followcrom/dad_bot",  # This could be a Docker Hub public image or an Amazon ECR image
+            'ports': {
+                '80': 'HTTP'
+            },
+            # Include other configuration as necessary
+        }
+    },
+    'publicEndpoint': {
+        'containerName': 'your-container-name',
+        'containerPort': 80,
+        'healthCheck': {
+            'path': '/',
+            # Other health check configurations
+        }
+    }
+}
+
+response = lightsail.create_container_service_deployment(
+    serviceName="dad-bot-cont",
+    containers=deployment['containers'],
+    publicEndpoint=deployment['publicEndpoint']
 )
